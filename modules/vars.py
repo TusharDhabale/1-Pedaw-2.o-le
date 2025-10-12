bf255c04fdc313cb94ee53c2702a79f01c52b990
Tushar Dhabale <128279576+tushardhab@users.noreply.github.com>
2025-10-12 19:48:11 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,4 +2,5 @@ import os
 
 api_id = "24798261"
 api_hash = "fef280037f5759eccc540c6d7a279a14"
-bot_token = "7436355167:AAHPxzrJ5oKxVooSc8Ine8gvfVg1X4ndE_I"
+bot_token = "8395223065:AAHO2NhWHiI6PVOHG5zw-_QkubKQ_3XURys"
+
